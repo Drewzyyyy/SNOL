bc3fd75433916b7024a25e4b58c3a65cb5689de1
Bea Totanes <36788217+beatricetotanes@users.noreply.github.com>
2021-06-08 21:21:31 +0800

    Added some changes

--- Calculation.cpp
+++ Calculation.cpp
@@ -74,34 +74,34 @@ string infixToPostfix(stack <char> stack, string infix) {
         if ((isdigit(infix[i])) || (infix[i] == '.')) {						// check if the current character is a digit or a period
             postfix += infix[i];											//append the current character to the postfix string
             if (infix[i + 1] == '.') continue;								//check if the next character of the current character is a period (if float), ignore
-            else if (!isdigit(infix[i + 1])) postfix += " ";	//check if the next character of the current character is not a digit (it may be two digits or more)
+            else if (!isdigit(infix[i + 1])) postfix += " ";	            //check if the next character of the current character is not a digit (it may be two digits or more)
         }
-        else if (infix[i] == ' ') {								//check if the current character is a space then ignore
+        else if (infix[i] == ' ') {								            //check if the current character is a space then ignore
             continue;
         }
-        else if (infix[i] == '(') {								//check if it is an open parentheses
-            stack.push(infix[i]);							    //push the open parentheses
+        else if (infix[i] == '(') {								            //check if it is an open parentheses
+            stack.push(infix[i]);							                //push the open parentheses
         }
-        else if (infix[i] == ')') {								//check if it is a closed parentheses
-            while ((stack.top() != '(') && (!stack.empty())) {	//loop until it meets the open parentheses or the stack is empty
+        else if (infix[i] == ')') {								            //check if it is a closed parentheses
+            while ((stack.top() != '(') && (!stack.empty())) {	            //loop until it meets the open parentheses or the stack is empty
                 char temp = stack.top();
-                postfix += temp;							    //append the top of the stack to the postfix string
-                postfix += " ";									// this will add spaces after each append in the postfix string
+                postfix += temp;							                //append the top of the stack to the postfix string
+                postfix += " ";									            // this will add spaces after each append in the postfix string
                 stack.pop();
             }
-            if (stack.top() == '(') {							//check if it encounters an open parentheses then pop
+            if (stack.top() == '(') {							            //check if it encounters an open parentheses then pop
                 stack.pop();
             }
         }
         else { 																//if the current infix[i] is operator
-            if (stack.empty()) {												//checks if stack is empty then push the current infix[i]
+            if (stack.empty()) {									    	//checks if stack is empty then push the current infix[i]
                 stack.push(infix[i]);
             }
             else {															//checks the precedence and of the top of the stack and the current infix[i]
                 if (precedence(infix[i]) > precedence(stack.top())) {		//push to the stack the current infix[i] if the precedence of the top of the stack is less than the current character
                     stack.push(infix[i]);
                 }
-                else if (precedence(infix[i]) == precedence(stack.top())) { 	//check if the precedence of the current character is equal to the stack top 
+                else if (precedence(infix[i]) == precedence(stack.top())) { //check if the precedence of the current character is equal to the stack top
                     postfix += stack.top();									//append the top of the stack to the postfix string
                     postfix += " ";
                     stack.pop();
@@ -118,7 +118,7 @@ string infixToPostfix(stack <char> stack, string infix) {
             }
         }
     }
-    while (!stack.empty()) {													//if there are remaining operators in the stack pop it
+    while (!stack.empty()) {											    //if there are remaining operators in the stack pop it
         postfix += stack.top();
         postfix += " ";
         stack.pop();
