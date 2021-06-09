5384cd3fab97f0d5a5fdaac937f15f0568e1434c
KristineKeithNapolitano <60454678+KristineKeithNapolitano@users.noreply.github.com>
2021-06-09 21:13:44 +0800

    Add files via upload

--- Calculation.h
+++ Calculation.h
@@ -9,3 +9,6 @@ int precedence(char value);
 string infixToPostfix(stack <char> stack, string infix);
 bool checkError(string postfix);
 bool checkType(string postfix);
+bool ifInt(float num);
+string evaluateIntPostfix(stack <float> mystack, string postfix);
+string evaluateFloatPostfix(stack <float> mystack, string postfix);
